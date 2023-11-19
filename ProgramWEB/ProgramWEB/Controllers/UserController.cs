47ad683065af8b864bc1433b09d9eeed2fa72bb9
Thuong <96791491+nguyenvanthuong10112003@users.noreply.github.com>
2023-11-20 02:57:27 +0700

    first commit

---
+++ ProgramWEB/ProgramWEB/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgramWEB.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult CheckLogin(string username, string password, bool remember, string author) {
+            return Json(new {});
+        }
+        public ActionResult ForgetPassword()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
